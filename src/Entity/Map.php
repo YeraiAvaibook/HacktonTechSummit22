6590143ca9894f2adc3d1dfac796c0c8a967e61c
Jorge Lasmarias <jorge.lasmarias@avaibook.com>
2022-09-15 15:53:07 +0200

    HacktonTechSummit22: Zombie and map entity

---
+++ src/Entity/Map.php
@@ -0,0 +1,36 @@
+<?php
+
+class Map {
+
+    private int $width;
+
+    private int $height;
+
+    public function __construct(int $width, int $height)
+    {
+        $this->width = $width;
+        $this->height = $height;
+    }
+
+
+    public function getWidth(): int
+    {
+        return $this->width;
+    }
+
+    public function setWidth(int $width)
+    {
+        $this->width = $width;
+    }
+
+    public function getHeight(): int
+    {
+        return $this->height;
+    }
+
+    public function setHeight(int $height)
+    {
+        $this->height = $height;
+    }
+
+}
---
+++ src/Entity/Zombie.php
@@ -0,0 +1,48 @@
+<?php
+
+class Zombie {
+
+    private string $name;
+
+    private int $coordinateX;
+
+    private int $coordinateY;
+
+    public function __construct(string $name, int $coordinateX, int $coordinateY)
+    {
+        $this->name = $name;
+        $this->coordinateX = $coordinateX;
+        $this->coordinateY = $coordinateY;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public function getCoordinateX(): int
+    {
+        return $this->coordinateX;
+    }
+
+    public function setCoordinateX(int $coordinateX)
+    {
+        $this->coordinateX = $coordinateX;
+    }
+
+    public function getCoordinateY(): int
+    {
+        return $this->coordinateY;
+    }
+
+    public function setCoordinateY(int $coordinateY)
+    {
+        $this->coordinateY = $coordinateY;
+    }
+
+}
