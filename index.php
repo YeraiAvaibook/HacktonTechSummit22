d0219363dcabf6e716c355605eaf4aea8df79783
Yerai Gonzalez <yerai.gonzalez@avaibook.com>
2022-09-15 17:10:59 +0200

    Print survivors and zombies.

--- index.php
+++ index.php
@@ -1,13 +1,89 @@
 <?php
-    function createBoard($size = 6)
+
+    $gameData = [
+        'boardSize' => '6',
+        'survivors' => [
+            [
+                'name' => 'Nano',
+                'live' => 3,
+                'exp' => 1,
+                'items' => [
+                    'leftHand' => null,
+                    'rightHand' => 'machete',
+                    'backpack' => [
+                        'rubber-duck'
+                    ],
+                ],
+                'positionX' => 2,
+                'positionY' => 4,
+            ],
+            [
+                'name' => 'Nino',
+                'live' => 3,
+                'exp' => 3,
+                'items' => [
+                    'leftHand' => 'knife',
+                    'rightHand' => null,
+                    'backpack' => null,
+                ],
+                'positionX' => 5,
+                'positionY' => 5,
+            ],
+        ],
+        'zombies' => [
+            [
+                'name' => 'zombie1',
+                'live' => 1,
+                'positionX' => 2,
+                'positionY' => 1,
+            ],
+            [
+                'name' => 'zombie2',
+                'live' => 1,
+                'positionX' => 3,
+                'positionY' => 4,
+            ],
+            [
+                'name' => 'zombie3',
+                'live' => 1,
+                'positionX' => 5,
+                'positionY' => 3,
+            ],
+        ],
+    ];
+
+    function createBoard($gameData)
     {
+        $boardSize = $gameData['boardSize'];
+        $survivors = $gameData['survivors'];
+        $zombies = $gameData['zombies'];
+
         $board = '<table>';
 
-        for ($row = 0; $row < $size; $row++) {
+        for ($row = 0; $row < $boardSize; $row++) {
             $board .= '<tr>';
 
-            for ($column = 0; $column < $size; $column++) {
-                $board .= '<td data-row="'.$row.'" data-column="'.$column.'"></td>';
+            for ($column = 0; $column < $boardSize; $column++) {
+                $board .= '<td class="tile'.rand(1, 3).'" data-row="'.$row.'" data-column="'.$column.'">';
+
+                $survivorPrinted = false;
+
+                foreach ($survivors as $key => $survivor) {
+                    if ($survivor['positionX'] == $column && $survivor['positionY'] == $row) {
+                        $board .= '<img src="assets/images/survivor'.$key.'.png" />';
+                        $survivorPrinted = true;
+                    }
+                }
+
+                if (!$survivorPrinted) {
+                    foreach ($zombies as $key => $zombie) {
+                        if ($zombie['positionX'] == $column && $zombie['positionY'] == $row) {
+                            $board .= '<img src="assets/images/zombie'.$key.'.png" />';
+                        }
+                    }
+                }
+
+                $board .= '</td>';
             }
 
             $board .= '</tr>';
@@ -25,7 +101,7 @@
     </head>
     <body>
         <div id="application-container">
-            <?php echo createBoard(); ?>
+            <?php echo createBoard($gameData); ?>
         </div>
     </body>
 </html>
