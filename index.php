5637f239c6cc38d9648cf673b3d06874a67462e8
Yerai Gonzalez <yerai.gonzalez@avaibook.com>
2022-09-15 16:16:32 +0200

    Create board.

--- index.php
+++ index.php
@@ -1,2 +1,31 @@
 <?php
+    function createBoard($size = 6)
+    {
+        $board = '<table>';
 
+        for ($row = 0; $row < $size; $row++) {
+            $board .= '<tr>';
+
+            for ($column = 0; $column < $size; $column++) {
+                $board .= '<td data-row="'.$row.'" data-column="'.$column.'"></td>';
+            }
+
+            $board .= '</tr>';
+        }
+
+        $board .= '</table>';
+
+        return $board;
+    }
+?>
+<html lang="en">
+    <head>
+        <title>Hackaton Tech Summit 2022</title>
+        <link rel="stylesheet" href="src/css/index.css">
+    </head>
+    <body>
+        <div id="application-container">
+            <?php echo createBoard(); ?>
+        </div>
+    </body>
+</html>
