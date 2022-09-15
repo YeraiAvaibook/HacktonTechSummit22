169e7c109c6eb1c645070b47f9c2fd9a46df566b
cesar_velasco <cesar.velasco@avaibook.com>
2022-09-15 15:49:29 +0200

    Generate Supervisor and item entities

---
+++ src/Entity/Item.php
@@ -0,0 +1,37 @@
+<?php
+
+class Item
+{
+    private $name;
+    private $position;
+
+    public function __construct($name, $position)
+    {
+        $this->name = $name;
+        $this->position = $position;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this->name;
+    }
+
+    public function getPosition()
+    {
+        return $this->position;
+    }
+
+    public function setPosition($position)
+    {
+        $this->position = $position;
+
+        return $this->position;
+    }
+}
---
+++ src/Entity/Survivor.php
@@ -0,0 +1,80 @@
+<?php
+
+class Survivor
+{
+    private $name;
+    private $life;
+    private $level;
+    private $experience;
+    private $item;
+    private $position;
+
+    public function __construct($name, $life, $level, $experience, $item, $position){
+        $this->name = $name;
+        $this->life = $life;
+        $this->level = $level;
+        $this->experience = $experience;
+        $this->item = $item;
+        $this->position = $position;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getLife()
+    {
+        return $this->life;
+    }
+
+    public function setLife($life)
+    {
+        $this->life = $life;
+    }
+
+    public function getLevel()
+    {
+        return $this->level;
+    }
+
+    public function setLevel($level)
+    {
+        $this->level = $level;
+    }
+
+    public function getExperience()
+    {
+        return $this->experience;
+    }
+
+    public function setExperience($experience)
+    {
+        $this->experience = $experience;
+    }
+
+    public function getItem()
+    {
+        return $this->item;
+    }
+
+    public function setItem($item)
+    {
+        $this->item = $item;
+    }
+
+    public function getPosition()
+    {
+        return $this->position;
+    }
+
+    public function setPosition($position)
+    {
+        $this->position = $position;
+    }
+}
